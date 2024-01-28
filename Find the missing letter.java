b750dc5f4fe1e3e7f8f9d1829bcd17f550af6375
Joaoz89 <113314996+Joaoz89@users.noreply.github.com>
2024-01-28 02:03:42 -0300

    Create  Find the missing letter.java

---
+++ Find the missing letter.java
@@ -0,0 +1,16 @@
+ public static char findMissingLetter(char[] array){
+    char expectableLetter = array[0];
+    for(char letter : array){
+      if(letter != expectableLetter) break;
+      expectableLetter++;
+    }
+    return expectableLetter;
+  }
+=============================================================
+
+  char expectableLetter = array[0];
+    for(char letter : array){
+      if(letter != expectableLetter) break;
+      expectableLetter++;
+    }
+    return expectableLetter;
